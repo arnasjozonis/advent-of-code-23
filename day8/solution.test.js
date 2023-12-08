7d58f7ec2512d33743a402b2b7f69385b3ad2f53
arnasjozonis <arnas.jozonis@gmail.com>
2023-12-08 13:27:22 +0200

    :pukeemoji: solution for part 2

--- day8/solution.test.js
+++ day8/solution.test.js
@@ -17,6 +17,17 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`);
 
+const example3 = parseString(`LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`)
+
 const data = parseTask('day8');
 
 
@@ -31,6 +42,14 @@ describe('desert map solution', () => {
 
   it('should return answer for input', () => {
     expect(solution(data)).toEqual(18157);
+  });
+
+  it('should return 2 for example data', () => {
+    expect(solutionV2(example3)).toEqual(6);
+  });
+
+  it('should return answer V2 for input', () => {
+    expect(solutionV2(data)).toEqual(14299763833181);
   });
 
 });
--- day8/solution.ts
+++ day8/solution.ts
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 type Node = [string, string];
 type Direction = 'L' | 'R';
 const step = {
@@ -8,20 +6,20 @@ const step = {
 }
 const words = RegExp(/\w{3}/, 'g');
 
-const NodeMap = new Map<string, Node>();
+let NodeMap: Record<string, Node> = {};
 
 const parseNode = (nodeStr: string): Node => {
   const [key, left, right] = [...nodeStr.matchAll(words)].map(x => x[0]);
 
   const node: Node = [left, right];
 
-  NodeMap.set(key, node);
+  NodeMap[key] = node;
 
   return node;
 }
 
 const solution = ([instructions, ...mapStrs]: string[]) => {
-  NodeMap.clear();
+  NodeMap = {};
   let steps = 0;
   mapStrs.forEach(parseNode);
   const instructionsLength = instructions.length;
@@ -31,7 +29,7 @@ const solution = ([instructions, ...mapStrs]: string[]) => {
   while(true) {
     steps++;
 
-    const node = NodeMap.get(currentStep) || [];
+    const node = NodeMap[currentStep] || [];
     const direction = instructions[currentDirectionIndex % instructionsLength] as Direction;
     currentStep = node[step[direction]];
 
@@ -46,6 +44,42 @@ const solution = ([instructions, ...mapStrs]: string[]) => {
   return steps;
 }
 
-const solutionV2 = (input: string[]) => solution(input);
+const solutionV2 = ([instructions, ...mapStrs]: string[]) => {
+  NodeMap = {};
+  let steps = 0;
+  mapStrs.forEach(parseNode);
+  const instructionsLength = instructions.length;
+  const starts = Object.keys(NodeMap).filter(x => x[2] === 'A');
+
+  let currentDirectionIndex = 0;
+  let repetions = starts.map(x => 0);
+  while (true) {
+      steps++;
+      const direction = instructions[currentDirectionIndex % instructionsLength] as Direction;
+      currentDirectionIndex++;
+      for (let i = 0; i < starts.length; i++) {
+          const next = NodeMap[starts[i]][step[direction]];
+          if(next[2] === 'Z' && repetions[i] === 0) {
+              repetions[i] = steps;
+          }
+          starts[i] = next;
+      }
+
+      if (repetions.every(x => x > 0)) {
+          break;
+      }
+  }
+  
+  const max = Math.max(...repetions);
+  let result = max;
+  while(true) {
+      if(repetions.every(x => result % x === 0)) {
+          break;
+      }
+      result += max;
+  }
+
+  return result;
+};
 
 export {  solution, solutionV2 };
