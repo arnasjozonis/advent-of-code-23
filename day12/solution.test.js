db2eff77d8ab2d544c840355b36a098d07dd698a
arnasjozonis <arnas.jozonis@gmail.com>
2023-12-16 20:20:57 +0200

    wip: day12 nightmare

--- day12/solution.test.js
+++ day12/solution.test.js
@@ -1,8 +1,8 @@
 
-import { solution, lineValid } from './solution';
+import { solution } from './solution';
 import { parseTask, parseString } from '../utils';
 const example0 = parseString(`?###???????? 3,2,1`);
-const example16 = parseString(`?###???????? 3,2,1`);
+const example16 = parseString(`.??..??...?##. 1,1,3`);
 const example1 = parseString(`?#?#?#?#?#?#?#? 1,3,1,6`);
 
 const example = parseString(`???.### 1,1,3
@@ -12,37 +12,40 @@ const example = parseString(`???.### 1,1,3
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1`);
 
+const tst = parseString(`
+????????..??????? 1,2,1,3,1`);
+
 const data = parseTask('day12');
 
-describe('Hot springs lineValid', () => {
-  it('should return true for valid inputs data', () => {
-    expect(lineValid('#.#.###', [1,1,3])).toEqual(1);
-    expect(lineValid('.#.###.#.######', [1,3,1,6])).toEqual(1);
-    expect(lineValid('####.#...#...', [4, 1, 1])).toEqual(1);
-  });
+// describe('Hot springs lineValid', () => {
+//   it('should return true for valid inputs data', () => {
+//     expect(lineValid('#.#.###', [1,1,3])).toEqual(1);
+//     expect(lineValid('.#.###.#.######', [1,3,1,6])).toEqual(1);
+//     expect(lineValid('####.#...#...', [4, 1, 1])).toEqual(1);
+//   });
 
-  it('should return false for invalid inputs data', () => {
-    expect(lineValid('##..###', [1,1,3])).toEqual(0);
-    expect(lineValid('###############', [1,3,1,6])).toEqual(0);
-    expect(lineValid('####.....##..', [4, 1, 1])).toEqual(0);
-  });
-});
+//   it('should return false for invalid inputs data', () => {
+//     expect(lineValid('##..###', [1,1,3])).toEqual(0);
+//     expect(lineValid('###############', [1,3,1,6])).toEqual(0);
+//     expect(lineValid('####.....##..', [4, 1, 1])).toEqual(0);
+//   });
+// });
 
 describe('Hot springs solution', () => {
-  // it('should return 10 for example0 data', () => {
-  //   expect(solution(example16)).toEqual(10);
-  // });
-
-  it('should return 10 for example0 data', () => {
-    expect(solution(example0)).toEqual(10);
+  it('should return 4 for example0 data', () => {
+    expect(solution(example16)).toEqual(4);
   });
+
+  // it('should return 10 for example0 data', () => {
+  //   expect(solution(example0)).toEqual(10);
+  // });
 
   // it('should return 21 for example data', () => {
   //   expect(solution(example)).toEqual(21);
   // });
 
   // it('should return answer for input data', () => {
-  //   expect(solution(data)).toEqual(6852);
+  //   expect(solution(tst, true)).toEqual(6852);
   // });
 
   // it('should return 16 for example data', () => {
--- day12/solution.ts
+++ day12/solution.ts
@@ -1,71 +1,77 @@
-const binaryToSpring = {
-  '0': '#',
-  '1': '.',
-} as const
+import { identity } from "lodash";
 
-const extractUnknownsIndices = (line: string) => {
+const extractCharIndices = (line: string, char: string) => {
   const indices = [];
   for(let i = 0; i < line.length; i++) {
-    if(line[i] === '?') {
+    if(line[i] === char) {
       indices.push(i);
     }
   }
   return indices;
 }
 
-// const lineValid = (line: string, damaged: number[]) => {
-//   let dmgIdx = 0;
-//   let dmgCounter = 0;
-//   for(let charIdx = 0; charIdx < line.length; charIdx++) {
-//     if(line[charIdx] === '#') {
-//       dmgCounter++;
-//     };
-
-//     if(dmgCounter === damaged[dmgIdx]) {
-//       if(line[charIdx + 1] !== '#') {
-//         dmgIdx++;
-//         dmgCounter = 0;
-//       } else {
-//         return 0;
-//       }
-//     }
-//   }
-
-//   return 1;
-// }
-
-const lineValid = (line: string, damaged: number[]) => {
-  let valid = 1;
-  let checkedIdx = 0;
-
+const solutionExists = (line: string, damaged: number[] = []) => {
+ 
+  const unknownIndices = extractCharIndices(line, '?');
+  const knownIndices = extractCharIndices(line, '#');
   const totalDamaged = damaged.reduce((acc, curr) => acc + curr, 0);
+  const [sequence] = damaged;
 
-  // check if line has enough # (total damaged) symbols
+  if(knownIndices.length + unknownIndices.length < sequence) {
+    console.log(`Not enough space ${knownIndices.length + unknownIndices.length} < ${sequence}`)
+    return false;
+  }
 
-  if(line.split('').filter(s => s === '#').length !== totalDamaged) {
+  if(totalDamaged > knownIndices.length + unknownIndices.length) {
+    console.log(`Damaged ${totalDamaged} > ${knownIndices.length + unknownIndices.length}`)
+    return false;
+  }
+
+  return true;
+}
+
+let globalCounter = 0;
+
+const solveLine = ({ line, damaged }: { line: string, damaged: number[] }): number => {
+  globalCounter++;
+  // if(globalCounter > 1000) {
+  //   throw new Error('Too many iterations');
+  // }
+  const [sequence, ...rest] = damaged;
+
+  if(!sequence) {
+    return line.includes('#') ? 0 : 1;
+  }
+
+  if(!line) {
+    return damaged.length > 0 ? 0 : 1;
+  }
+
+  if(!solutionExists(line, damaged)) {
     return 0;
   }
+  
+  const unknownIndices = extractCharIndices(line, '?');
+  const knownIndices = extractCharIndices(line, '#');
 
+  const start = Math.min(unknownIndices[0], ...knownIndices);
+  
+  const mask = line.slice(start, start + sequence);
 
-  for(let damagedSequence of damaged) {
-    const exists = line.substring(checkedIdx).match(new RegExp(`#{${damagedSequence}}`));
-    
-    if(!exists || exists.index === undefined) {
-      valid = 0;
-      break;
-    }
-
-    checkedIdx += exists.index + damagedSequence;
-    const nextSymbol = line[checkedIdx];
-    
-    if(nextSymbol === '#') {
-      valid = 0;
-      break;
-    }
-
+  const nextSolutionExists = solutionExists(line.slice(1), damaged);
+  const step =  nextSolutionExists ? solveLine({ line: line.slice(1), damaged }) : 0;
+  if(mask.includes('.') || line[start + sequence] === '#') {
+    return step;
   }
 
-  return valid;
+  if(!(start + sequence + 1)) {
+    throw new Error(`No next char ${start} ${sequence} ${line}`);
+  }
+
+  
+
+  const jump = solveLine({ line: line.slice(mask.length + 2), damaged: rest });
+  return 1 * jump + step;
 }
 
 
@@ -73,109 +79,6 @@ const parseInputLine = (input: string) => {
   const [line, damaged] = input.split(' ');
   const damagedNumbers = damaged.split(',').map(Number);
   return { line, damaged: damagedNumbers };
-}
-
-const solveLine = (line: string, damaged: number[]) => {
-  
-  const editableLine = line.split('');
-  const damagedCount = damaged.reduce((acc, curr) => acc + curr, 0);
-  const knownDamagedCount = editableLine.filter(d => d === '#').length;
-  const unknownDamagedCount = damagedCount - knownDamagedCount;
-  const unknownsIndices = extractUnknownsIndices(line);
-  const unknownsLength = unknownsIndices.length;
-  const variants = 2 ** unknownsLength;
-  let validVariants = 0;
-  let variant = 0;
-  while(variant < variants) {
-    const binary = variant.toString(2).padStart(unknownsLength, '0').split('') as ('0' | '1')[];
-    binary.forEach((bit, idx) => {
-      const symbol = binaryToSpring[bit];
-      editableLine[unknownsIndices[idx]] = symbol;
-    });
-    
-    if(lineValid(editableLine.join(''), damaged) > 0) {
-      // console.log(`Variant ${variant}/${variants}: ${editableLine.join('')} / ${line} (${damaged})`);
-      validVariants++;
-    }
-
-    variant++;
-  }
-  return validVariants;
-}
-
-const findNextValid = (line: string, damaged: number[]) => {
-  const editableLine = line.split('');
-  const unknownsIndices = extractUnknownsIndices(line);
-  const unknownsLength = unknownsIndices.length;
-  const variants = 2 ** unknownsLength;
-  let variant = 0;
-  while(variant < variants) {
-    const binary = variant.toString(2).padStart(unknownsLength, '0').split('') as ('0' | '1')[];
-    binary.forEach((bit, idx) => {
-      const symbol = binaryToSpring[bit];
-      editableLine[unknownsIndices[idx]] = symbol;
-    });
-
-    const res = editableLine.join('');
-    
-    if(lineValid(res, damaged) > 0) {
-      return res;
-    }
-
-    variant++;
-  }
-  return null;
-};
-
-
-const solveLineRec2 = (line: string, damaged: number[], checkedOffset: number = 0): Set<string> => {
-  const res = new Set<string>();
-  const unknownIndices = extractUnknownsIndices(line);
-
-  console.log(`Solving ${line} / ${damaged} / ${checkedOffset} =>>`);
-  if(checkedOffset > line.length) {
-    return res;
-  }
-  
-  if(line.length === 0 || damaged.length === 0 || unknownIndices.length === 0) {
-    return res;
-  }
-
-  const inputEnd = line.slice(checkedOffset);
-  const inputStart = line.slice(0, checkedOffset).replaceAll('?', '.');
-
-  const nextValid = findNextValid(inputStart.replaceAll('#', '.').concat(inputEnd), damaged);
-//   console.log(`
-//   line: ${line} 
-//   nextValid: ${nextValid}
-//   input: ${inputStart.replaceAll('?', '.').concat(inputEnd)}
-//  `)
-  if(!nextValid) {
-    return res;
-  }
-
-  const solutionStart = nextValid.slice(0, checkedOffset);
-  const solutionEnd = nextValid.slice(checkedOffset);
-
-  const result = inputStart + solutionEnd;
-  console.log(`Result: ${nextValid}`);
-  res.add(nextValid);
-  
-  const [damagedSequence, ...rest] = damaged;
-
-  const match = nextValid.match(new RegExp(`#{${damagedSequence}}`))!;
-  const matchIdx = match.index!;  
-  const nextValidSplitIdx = matchIdx + damagedSequence + 1;  
- 
-  
-  
-  const jumpRes = solveLineRec2(line, rest, nextValidSplitIdx);
-  jumpRes.forEach((r) => res.add(r));
-
-  // const stepRes = solveLineRec2(line, damaged, checkedOffset + 1);
-  // stepRes.forEach((r) => res.add(r));
-
-  return res;
 }
 
 const unfold = (input: string[]) => 
@@ -188,10 +91,11 @@ const unfold = (input: string[]) =>
 
 const solution = (input: string[], withUnfolds = false) => {
   const mappedInput = withUnfolds ? unfold(input) : input.map(parseInputLine);
-  const results = mappedInput.map(({ line, damaged }) => solveLineRec2(line, damaged).size);
-
+  const results = mappedInput.map(solveLine);
+  console.log({globalCounter})
+  
   return results.reduce((acc, curr) => acc + curr, 0);
 };
 
 
-export {  solution, lineValid };
+export {  solution };
